4a5a9051d144366aface369b4fc0896caecb4e0a
Juan F Carlos <juanfcarlos.93@gmail.com>
2018-05-27 20:11:57 -0300

    Script C#
    Inserido o Script e feito o link com o GameObject maincamera

---
+++ HelloWordC#Unity/Assets/HelloWorld.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("This is my First C# Script - Hello World - Alô Mundo");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
